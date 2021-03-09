7e19eb8ff1f618896211a567649308145916eb53
Валентин <valgo>
2021-03-10 00:04:47 +0200

    test

--- python_tg_bot/tg_bot.py
+++ python_tg_bot/tg_bot.py
@@ -7,7 +7,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
+'gdhdh'
 logger = logging.getLogger(__name__)
 
 
