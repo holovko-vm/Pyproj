28714714fa54613e42995d18644fbf5973f6645a
Валентин <valgo>
2021-03-14 01:02:09 +0200

    some experiments

--- python_tg_bot/tg_bot.py
+++ python_tg_bot/tg_bot.py
@@ -7,6 +7,14 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
+
+
+def commands(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text(f'принято команду - {update.message.text}')
+def kill(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'We kill you')
+commandsr = ['rakuvaln9', 'rak', 'givno', 'kill', 'commands']
 token = "1602418380:AAGfEIP4Z4rZEZieQjueQNWf_C0r7FW0h_A"
 logger = logging.getLogger(__name__)
 class My_tg_bot:
@@ -14,25 +22,35 @@ class My_tg_bot:
         self.updater = Updater(token=token)
         self.dispatcher = self.updater.dispatcher
 
-    def commands(self, update: Update, context: CallbackContext) -> None:
-        """Send a message when the command /start is issued."""
-        update.message.reply_text(f'принято команду - {update.message.text}')
+
+
+
 
     def echo(self, update: Update, context: CallbackContext) -> None:
         """Echo the user message."""
         update.message.reply_text(update.message.text)
 
-    def add_handlers(self, command):
-        self.dispatcher.add_handler(CommandHandler(f'{command}', self.commands))
+
+    def add_command_handlers(self, *args):
+        for _ in args:
+            if _ == 'kill':
+                self.dispatcher.add_handler(CommandHandler(f'{_}', kill))
+            self.dispatcher.add_handler(CommandHandler(f'{_}', commands))
+
+
+    def add_handlers(self):
         self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.echo))
 
-
-
-    def run(self):
-        self.add_handlers(command='start')
+    def run(self, args):
+        for _ in args:
+            self.add_command_handlers(_)
+        self.add_handlers()
         self.updater.start_polling()
         self.updater.idle()
 
+
+
+
 if __name__ == '__main__':
     bot = My_tg_bot(token=token)
-    bot.run()
+    bot.run(commandsr)
