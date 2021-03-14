496f5dc1fb638ef8d04c14dd410846352ccc1f95
Валентин <valgo>
2021-03-14 21:35:34 +0200

    Додав пояснення і зробив зрозуміліший неймінг, поставив задачі

--- python_tg_bot/tg_bot.py
+++ python_tg_bot/tg_bot.py
@@ -2,12 +2,13 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from token_bot import token
-# Enable logging
+
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
-
+logger = logging.getLogger(__name__)
+# TODO написати коректне логування і тести
 def echo(update: Update, context: CallbackContext) -> None:
     """Ехо-відповідь користувачу"""
     update.message.reply_text(update.message.text)
@@ -19,15 +20,19 @@ def commands(update: Update, context: CallbackContext) -> None:
 def kill(update: Update, context: CallbackContext) -> None:
     """Фан-функція, формат виклику /kill 'name' """
     update.message.reply_text(f'We will kill {update.message.text[6:]} for you')
+# """Список використовуваних ботом функцій """TODO винести функції в окремий файл і імпортувати згідно цього списку
 
 COMMAND_LIST = ['kill', 'commands', 'echo']
-logger = logging.getLogger(__name__)
+
 
 class My_tg_bot:
     def __init__(self, token):
+        """Створюємо Об'єкт, що слідкує за новинами"""
         self.updater = Updater(token=token)
+        """Створюємо Об'єкт, який направляє новину відповідному обробнику"""
         self.dispatcher = self.updater.dispatcher
 
+    """Метод створення обробників згідно списку команд з COMMAND_LIST"""
     def add_handlers(self, command):
         try:
             if command == 'echo':
@@ -36,17 +41,15 @@ class My_tg_bot:
             _ = eval(command)
             self.dispatcher.add_handler(CommandHandler(command, _))
         except NameError as namerr:
-            print(f'Отсутствует такая функция {namerr}')
-
+            print(f'Невідома функція - {namerr}')
 
     def run(self, args):
+        """Створюємо обробників"""
         for _ in args:
             self.add_handlers(_)
+        """Слухаємо сервер"""
         self.updater.start_polling()
         self.updater.idle()
-
-
-
 
 if __name__ == '__main__':
     bot = My_tg_bot(token=token)
