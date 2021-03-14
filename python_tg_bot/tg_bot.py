885702914562484d02035e361d1715d8464936cb
Валентин <valgo>
2021-03-14 21:07:10 +0200

    refaktor handlers and functions

--- python_tg_bot/tg_bot.py
+++ python_tg_bot/tg_bot.py
@@ -1,5 +1,5 @@
 import logging
-
+from token import token
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -8,39 +8,40 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
+def echo(update: Update, context: CallbackContext) -> None:
+    """Ехо-відповідь користувачу"""
+    update.message.reply_text(update.message.text)
 
 def commands(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    update.message.reply_text(f'принято команду - {update.message.text}')
+    """Список використовуваних ботом команд"""
+    update.message.reply_text(f'Список доступних команд - {COMMAND_LIST}')
+
 def kill(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'We kill you')
-commandsr = ['rakuvaln9', 'rak', 'givno', 'kill', 'commands']
-token = "1602418380:AAGfEIP4Z4rZEZieQjueQNWf_C0r7FW0h_A"
+    """Фан-функція, формат виклику /kill 'name' """
+    update.message.reply_text(f'We will kill {update.message.text[6:]} for you')
+
+COMMAND_LIST = ['kill', 'commands', 'echo']
 logger = logging.getLogger(__name__)
+
 class My_tg_bot:
     def __init__(self, token):
         self.updater = Updater(token=token)
         self.dispatcher = self.updater.dispatcher
 
-    def echo(self, update: Update, context: CallbackContext) -> None:
-        """Echo the user message."""
-        update.message.reply_text(update.message.text)
+    def add_handlers(self, args):
+        try:
+            if args == 'echo':
+                self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+                return
+            _ = eval(args)
+            self.dispatcher.add_handler(CommandHandler(args, _))
+        except NameError as namerr:
+            print(f'Отсутствует такая функция {namerr}')
 
-
-    def add_command_handlers(self, *args):
-        for _ in args:
-            if _ == 'kill':
-                self.dispatcher.add_handler(CommandHandler(f'{_}', kill))
-            self.dispatcher.add_handler(CommandHandler(f'{_}', commands))
-
-
-    def add_handlers(self):
-        self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.echo))
 
     def run(self, args):
         for _ in args:
-            self.add_command_handlers(_)
-        self.add_handlers()
+            self.add_handlers(_)
         self.updater.start_polling()
         self.updater.idle()
 
@@ -49,4 +50,4 @@ class My_tg_bot:
 
 if __name__ == '__main__':
     bot = My_tg_bot(token=token)
-    bot.run(commandsr)
+    bot.run(COMMAND_LIST)
