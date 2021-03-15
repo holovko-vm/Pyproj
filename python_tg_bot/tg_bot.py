1d524e35637f4fa2673a6be67ba65b6510f90981
САС <valikgolovko@ukr.net>
2021-03-15 11:33:27 +0200

    Додав логування і рефактор коду для розуміння

--- python_tg_bot/tg_bot.py
+++ python_tg_bot/tg_bot.py
@@ -3,18 +3,23 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from token_bot import token
 import bot_commands
+
+"""Додаємо логування"""
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d.%m.%Y %I:%M',
+    level=logging.INFO,
+    handlers=[logging.FileHandler('tg_bot.log', 'w', 'utf-8')]
 )
-logger = logging.getLogger(__name__)
-# TODO написати коректне логування і тести
 
 """Список використовуваних ботом функцій """
-COMMAND_LIST = ['kill', 'commands', 'echo']
+COMMAND_LIST = ['kill', 'commands', 'echo','givno']
+logging.debug(f'Стартуємо з функціями {COMMAND_LIST}')
 
 class My_tg_bot:
     def __init__(self, token):
-        """Створюємо Об'єкт, що слідкує за новинами"""
+        """Створюємо Об'єкт, що слідкує за новинами,
+        Вставте токен Вашого бота token_bot.py.default --> token_bot.py >>token = 'Токен Вашого бота'
+        """
         self.updater = Updater(token=token)
         """Створюємо Об'єкт, який направляє новину відповідному обробнику"""
         self.dispatcher = self.updater.dispatcher
@@ -27,8 +32,8 @@ class My_tg_bot:
                     add_handler(MessageHandler(Filters.text & ~Filters.command, getattr(bot_commands, 'echo')))
                 return
             self.dispatcher.add_handler(CommandHandler(command, getattr(bot_commands, command)))
-        except NameError as namerr:
-            print(f'Невідома функція - {namerr}')
+        except AttributeError:
+            logging.error(f'Невідома функція - {command}, додайте її до файлу bot_commands.py')
 
     def run(self, args):
         """Створюємо обробників"""
@@ -40,4 +45,5 @@ class My_tg_bot:
 
 if __name__ == '__main__':
     bot = My_tg_bot(token=token)
+    """Запускаємо бота та передаємо йому список команд, які буде використовувати бот"""
     bot.run(COMMAND_LIST)
