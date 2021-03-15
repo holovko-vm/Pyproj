385e4fc0215bddcded297a109c03fe328b5cfcbe
САС <valikgolovko@ukr.net>
2021-03-15 14:51:48 +0200

    Початок тестів 2

--- python_tg_bot/tests.py
+++ python_tg_bot/tests.py
@@ -1,6 +1,11 @@
 import  unittest
+from unittest import  TestCase
+from  unittest.mock import patch
 import python_tg_bot
 from python_tg_bot import tg_bot, bot_commands
 
-class Test1(unittest.TestCase):
-    pass
+class Test(TestCase):
+    def test1(self):
+        with patch('tg_bot.Updater'):
+            bot = tg_bot.My_tg_bot('')
+            print('some')
