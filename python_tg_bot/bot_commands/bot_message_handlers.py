e2ffaa4f4a12310cad404a00e57e699bf8178a98
САС <valikgolovko@ukr.net>
2021-03-23 10:02:09 +0200

    ну і де я зараз

--- python_tg_bot/bot_commands/bot_message_handlers.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-
-from telegram.ext import CallbackContext
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    """Ехо-відповідь користувачу"""
-    update.message.reply_text(update.message.text)
