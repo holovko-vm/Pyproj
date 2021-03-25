9b89ddbeeaeeedae4bf713f41b76e84b32a607d5
САС <valikgolovko@ukr.net>
2021-03-25 10:41:05 +0200

    +++

--- python_tg_bot/message_functions.py
+++ python_tg_bot/message_functions.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-message_functions_list =['echo']
+'''фільтри для хендлерів знаходяться в модулі {Filters}'''
+message_functions_dict ={'echo': Filters.text & ~Filters.command}
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Ехо-відповідь користувачу"""
