a93ccb2e4902cfef4853a491b3e9904ce3a97388
Oleh <olehbozhok@gmail.com>
2021-03-28 13:05:02 +0300

    registration scenario finished

--- python_tg_bot/command_functions.py
+++ python_tg_bot/command_functions.py
@@ -29,41 +29,8 @@ def kill(**kwargs):
 
 
 def registration(users_ctx, **kwargs):
-    users_ctx['user_state'] = 1
-    probe_pass = None
-    password = None
-    user_email = None
-    re_email = re.compile('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$')
 
     def echo_regist_1(update: Update, context: CallbackContext) -> None:
-        nonlocal probe_pass, password, user_email
-        print("text ", update.message.text)
-        if users_ctx['user_state'] == 1:
-            user_email = update.message.text
-            if re_email.search(user_email):
-                users_ctx['user_state'] = 2
-                update.message.reply_text('Введіть password')
-            else:
-                users_ctx['user_state'] = 1
-                update.message.reply_text('Введіть email')
-
-        elif users_ctx['user_state'] == 2:
-            if len(update.message.text) >= 8:
-                update.message.reply_text('Підтвердіть password')
-                probe_pass = update.message.text
-                users_ctx['user_state'] = 3
-            else:
-                update.message.reply_text('Пароль повинен бути більше 8 літер')
-        elif users_ctx['user_state'] == 3:
-            if update.message.text == probe_pass:
-                password = update.message.text
-                update.message.reply_text('Реєстрація успішна!')
-                with open(file='python_tg_bot\\data_base.txt', mode='a', encoding='utf-8') as file:
-                    file.write(f'{user_email} : {password},')
-                users_ctx['user_state'] = 0
-            else:
-                update.message.reply_text(
-                    'Некоректний повторний пароль.\nВведіть password')
-                users_ctx['user_state'] = 2
-
+        users_ctx['user_state'] = 1
+        update.message.reply_text('Введіть email')
     return echo_regist_1
--- python_tg_bot/message_functions.py
+++ python_tg_bot/message_functions.py
@@ -12,6 +12,11 @@ def echo(update: Update, context: CallbackContext) -> None:
 
 
 def echo_for_meeting(users_ctx, **kwargs):
+    probe_pass = None
+    password = None
+    user_email = None
+    re_email = re.compile('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$')
+
     def echo_for_meeting(update: Update, context: CallbackContext) -> None:
         if users_ctx['user_state'] == 0:
             _date = '15 квітня'
@@ -35,45 +40,33 @@ def echo_for_meeting(users_ctx, **kwargs):
                         return
             else:
                 update.message.reply_text(DEFAULT_ANSWER)
-    return echo_for_meeting
 
-
-def echo_regist():
-    user_state = 0
-    probe_pass = None
-    password = None
-    user_email = None
-    re_email = re.compile('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$')
-
-    def echo_regist_1(update: Update, context: CallbackContext) -> None:
-        nonlocal user_state, probe_pass, password, user_email
-
-        if user_state == 0:
+        nonlocal probe_pass, password, user_email
+        if users_ctx['user_state'] == 1:
             user_email = update.message.text
             if re_email.search(user_email):
-                user_state = 2
+                users_ctx['user_state'] = 2
                 update.message.reply_text('Введіть password')
             else:
-                user_state = 0
-                update.message.reply_text('Введіть email')
+                users_ctx['user_state'] = 1
+                update.message.reply_text('Введіть коректний email')
 
-        elif user_state == 2:
+        elif users_ctx['user_state'] == 2:
             if len(update.message.text) >= 8:
                 update.message.reply_text('Підтвердіть password')
                 probe_pass = update.message.text
-                user_state = 3
+                users_ctx['user_state'] = 3
             else:
                 update.message.reply_text('Пароль повинен бути більше 8 літер')
-        elif user_state == 3:
+        elif users_ctx['user_state'] == 3:
             if update.message.text == probe_pass:
                 password = update.message.text
                 update.message.reply_text('Реєстрація успішна!')
                 with open(file='python_tg_bot\\data_base.txt', mode='a', encoding='utf-8') as file:
                     file.write(f'{user_email} : {password},')
-                user_state = 4
+                users_ctx['user_state'] = 0
             else:
                 update.message.reply_text(
                     'Некоректний повторний пароль.\nВведіть password')
-                user_state = 2
-
-    return echo_regist_1
+                users_ctx['user_state'] = 2
+    return echo_for_meeting
