5f2a6c6aaa90fb3ccb83956a3f2da65ab4eead90
САС <valikgolovko@ukr.net>
2021-04-09 11:45:36 +0300

    хз як замкнути пробпас поки що

--- python_tg_bot/message_functions.py
+++ python_tg_bot/message_functions.py
@@ -12,24 +12,32 @@ def echo_2(update,context):
     """Ехо-відповідь користувачу"""
     return update.message.reply_text('ха-ха бидлокод, моя взяла')
     
-def user_message_handler(users_ctx, **kwargs):  
-    def user_message_handler(update= Update, context= CallbackContext, user_ctx=users_ctx, *args, **kwargs):
-        print('я тута')
-        print(user_ctx)
-        if user_ctx['user_handler']==1:
+def user_message_handler(users_ctx, **kwargs):
+    
+    password = None
+    user_email = None
+    probe_pass = None
+    def user_message_handler(update=Update, context=CallbackContext,
+     users_ctx=users_ctx, probe_pass=probe_pass, *args, **kwargs):
+        
+        print('start')
+        print(f'start users_ctx - {users_ctx}')
+        if users_ctx['user_handler']==1:
             echo(update=update, context=context, givno=1)
-        if user_ctx['user_handler']==0:
-            echo_2(update=update, context=context)
-        print('і тута')
+        if users_ctx['user_handler']==0:
+            nonlocal password, user_email
+            print(f'до виклику пас такий - {probe_pass}')
+            echo_for_meeting(users_ctx, probe_pass, password, user_email, update, context)
+        print(f'після виклику пас такий - {probe_pass}')
+        print(f'end users_ctx - {users_ctx}')
+        print('end')
     return user_message_handler
 
 
     
 
-def echo_for_meeting(update: Update, context: CallbackContext) -> None:
-    probe_pass = None
-    password = None
-    user_email = None
+    
+def echo_for_meeting(users_ctx, probe_pass, password, user_email, update: Update, context: CallbackContext) -> None:
     re_email = re.compile('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$')
     if users_ctx['user_state'] == 0:
         _date = '15 квітня'
@@ -59,29 +67,32 @@ def echo_for_meeting(update: Update, context: CallbackContext) -> None:
         user_email = update.message.text
         if re_email.search(user_email):
             users_ctx['user_state'] = 2
-            update.message.reply_text('Введіть password')
+            return update.message.reply_text('Введіть password')
         else:
             users_ctx['user_state'] = 1
-            update.message.reply_text('Введіть коректний email')
-
+            return update.message.reply_text('Введіть коректний email')
     elif users_ctx['user_state'] == 2:
         if len(update.message.text) >= 8:
-            update.message.reply_text('Підтвердіть password')
             probe_pass = update.message.text
             users_ctx['user_state'] = 3
+            return probe_pass, update.message.reply_text('Підтвердіть password')
         else:
-            update.message.reply_text('Пароль повинен бути більше 8 літер')
+            return update.message.reply_text('Пароль повинен бути більше 8 літер')
     elif users_ctx['user_state'] == 3:
         if update.message.text == probe_pass:
             password = update.message.text
-            update.message.reply_text('Реєстрація успішна!')
+            print('Попав у блок реєстрації, проб-пас нарешті замкнувся')
             with open(file='python_tg_bot\\data_base.txt', mode='a', encoding='utf-8') as file:
                 file.write(f'{user_email} : {password},')
+                print('Запис вдався')
             users_ctx['user_state'] = 0
+            print('невже!!!!')
+            return update.message.reply_text('Реєстрація успішна!')
         else:
-            update.message.reply_text(
-                'Некоректний повторний пароль.\nВведіть password')
             users_ctx['user_state'] = 2
+            return update.message.reply_text(
+                'Некоректний повторний пароль.\nВведіть password')
+         
 
 
 
