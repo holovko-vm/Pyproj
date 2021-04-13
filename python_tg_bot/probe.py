7b5f0b0a164ddaccc8a6a13e6380ece21dd1e990
САС <valikgolovko@ukr.net>
2021-04-13 17:37:08 +0300

    розпарсив, змінив тег

--- python_tg_bot/probe.py
+++ python_tg_bot/probe.py
@@ -1,11 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
+
+target_site = 'www.youtube.com'
+target_site_str = f'{target_site}'
 response = requests.get('https://thehost.ua/ua/domains/whois')
-
 
 if response.status_code ==200:
   html_doc = BeautifulSoup(response.text, features='html.parser')
-  list_of = html_doc.find_all('span')
-  print(list_of)
+  list_of = html_doc.find_all('input')
+  for tag in list_of:
+    if tag.get(key = 'value') is not None:
+      tag.attrs['value'] = target_site_str
+# Розпарсив, змінив тег, щоб в ньому був необхідний сайт
+
+
+# TODO залити назад, натиснути кнопку пошуку, розпарсити необхідну інфу з отриманої сторінки
+
+  # for elem in list_of:
+  #   if '<input id="check-inp" name="domain"' in elem:
+  #     elem = f'<input id="check-inp" name="domain" placeholder="введіть ім'я для перевірки" type="text" value="{target_site}"/>'
